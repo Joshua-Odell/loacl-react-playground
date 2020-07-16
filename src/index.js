8e2f234f4273d785c47eb0b81bf6ed0c382da24b
Josh Odell <52717569+Joshua-Odell@users.noreply.github.com>
2020-07-16 09:17:50 -0500

    two checkpoints completed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+//import App from './App';
 import SplitCall from './Splits-Tags/SplitCall';
+import { BrowserRouter } from 'react-router-dom';
+import App from './Power-Toes/App'
+import AppLang from './lang-context/AppLang';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<AppLang />,
+ document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
